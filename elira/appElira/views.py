bf5ec6b838c0ef75116b58ad25dd52a15f45ef8c
Vladimír Hanhur <aqgz@email.cz>
2024-11-14 19:56:49 +0100

    update

--- elira/appElira/views.py
+++ elira/appElira/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request, 'appElira/index.html')
+
+def about_page(request):
+    return render(request, 'appElira/about.html')
